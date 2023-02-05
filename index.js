7bd4b09cb83690d143bdecc55a423981218da553
shohan-pherones <shohan.sub.56@gmail.com>
2023-02-05 12:45:05 +0600

    server and db initialized

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// port
+const port = process.env.PORT || 5000;
+
+// express app
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// listening
+app.listen(port, (req, res) => {
+  console.log(`Server running on port: ${port}`);
+});
+
+// mongodb
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established"))
+  .catch((error) => console.log("MongoDB connection failed,", error.message));
